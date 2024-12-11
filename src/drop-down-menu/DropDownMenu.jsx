9585b00eb65361f1b7212b13b8f865b3b9564418
samucop95 <samucop@icloud.com>
2024-12-11 15:58:12 +0100

    nueva barra de busqueda inteligente

--- src/drop-down-menu/DropDownMenu.jsx
+++ src/drop-down-menu/DropDownMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './DropDownMenu.css';  // Puedes crear un archivo CSS separado si lo prefieres
+import './DropDownMenu.css';
 
 function DropDownMenu({ cityList, onCitySelect }) {
     return (
@@ -10,8 +10,10 @@ function DropDownMenu({ cityList, onCitySelect }) {
                     onClick={() => onCitySelect(city)} 
                     key={`${city.lat}-${city.lon}`}
                 >
-                    <span className="city-name">{city.name}</span>
-                    {city.state && <span className="city-state">, {city.state}</span>}
+                    <span className="city-name">
+                        {city.local_names?.es || city.name},
+                        </span>
+                    {city.state && <span className="city-state">{city.state}</span>}
                     <span className="city-country"> ({city.country})</span>
                 </div>
             ))}
--- src/search-bar/SearchBar.jsx
+++ src/search-bar/SearchBar.jsx
@@ -1,18 +1,21 @@
-import Button from '../button/Button';
 import Input from '../input/Input';
 import { getGeoLocation } from '../utils/ApiCalls';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import DropDownMenu from '../drop-down-menu/DropDownMenu';
 import './SearchBar.css';
 
 function SearchBar({ onSubmit }) {
-    const [city, setCity] = useState('');
+    //const [city, setCity] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [cityList, setCityList] = useState([]);
 
-    const handleSearch = async (e) => {
-        e.preventDefault();
+    const searchTimeout = useRef(null);
+    const handleSearch = async (city) => {
+        if (!city.trim()) {
+            setCityList([]);
+            return;
+        }
         setLoading(true);
         setError(null);
 
@@ -26,6 +29,19 @@ function SearchBar({ onSubmit }) {
         }
     };
 
+    const handleInputChange = (e) => {
+        const newCity = e.target.value;
+
+        setCityList([]);
+        if (searchTimeout.current) {
+            clearTimeout(searchTimeout.current);
+        }
+
+        searchTimeout.current = setTimeout(() => {
+            handleSearch(newCity);
+        }, 500);
+    };
+
     const handleSubmit = city => {
         setCityList([]);
         onSubmit(city);
@@ -34,22 +50,18 @@ function SearchBar({ onSubmit }) {
     return (
         <>
             <div className="search-bar">
-                <form onSubmit={handleSearch}>
-                    <Input
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                        placeholder="Buscar ciudad..."
-                    />
-                    <Button loading={loading} />
-                </form>
-                {cityList.length > 0 && (
-                    <DropDownMenu 
-                        cityList={cityList}
-                        onCitySelect={handleSubmit}
-                    />
-                )}
+                <Input
+                    onChange={handleInputChange}
+                    placeholder="Buscar ciudad..."
+                />
             </div>
-            {error && (
+            {cityList.length > 0 && (
+                <DropDownMenu
+                    cityList={cityList}
+                    onCitySelect={handleSubmit}
+                />
+            )}
+            { error && (
                 <div className="error-message">{error}</div>
             )}
         </>
