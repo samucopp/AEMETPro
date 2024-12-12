a448e0188566c3a26509a12996cd1463d9ff037c
Jorge Pascual Fuentecilla <jorgepasf@gmail.com>
2024-12-12 18:47:08 +0100

    prueba

--- src/favorites/FavoritesList.jsx
+++ src/favorites/FavoritesList.jsx
@@ -1,11 +1,17 @@
 import FavoriteTemplate from "./FavoriteTemplate";
-export default function FavoritesList({ favorites }) {
+export default function FavoritesList({ favorites, OnCityClick }) {
     return (
         <div>
             <h2>Favoritos</h2>
             {favorites.length > 0 ? (
                 <div className="favorites-list">
                     {favorites.map((city, index) => (
+                        <div
+                        key={index}
+                        className="favorite-card"
+                        onClick={() => OnCityClick(city)}
+                        style={{cursor:"pointer"}}
+                        >
                         <FavoriteTemplate key={index} city={city}/>
                     ))}
                 </div>
